66f85494a8beca0b4fea171e83af051a858c2c55
Oh-HelloFriend <vpsouslinux@gmail.com>
2024-01-16 10:10:15 +0100

    build get player choice + put in lower case

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Computer Choice
 function getComputerChoice(){
     
     let rng = Math.random();
@@ -5,14 +6,22 @@ function getComputerChoice(){
     let computerChoice;
     
     if (rng <= (1/3)) {
-        computerChoice = "Rock"
+        computerChoice = "rock"
     } else if (rng <= (2/3)) {
-        computerChoice = "Paper"
+        computerChoice = "paper"
     } else{
-        computerChoice ="Scissors"
+        computerChoice ="scissors"
     }
     
     return computerChoice;
 }
+//
 
-console.log(getComputerChoice())
+// Get Player Choice and put it in lower case
+function getPlayerChoice(){
+    let playerChoice = prompt("Rock, paper or scissors ?").toLowerCase();
+}
+//
+// Make sure Player plays RPS, loop to player choice
+
+//
